db7c53621be1b83dfe25bb856385d686c25cfc8f
threeset3 <threeset3@gmail.com>
2015-02-14 16:16:46 -0600

    cleanup

--- Lab4/Carry-Lookahead/simulation/modelsim/rtl_work/@hex@driver/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity HexDriver is
-    port(
-        In0             : in     vl_logic_vector(3 downto 0);
-        Out0            : out    vl_logic_vector(6 downto 0)
-    );
-end HexDriver;
