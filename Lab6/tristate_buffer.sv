f1f6261ac69b0a4cc87b6f9c04adab0330aa00a9
elliequirini <gquirin2@illinois.edu>
2015-02-26 13:26:32 -0600

    changed tristate

--- Lab6/tristate_buffer.sv
+++ Lab6/tristate_buffer.sv
@@ -1,10 +1,4 @@
 module tristate_buffer(input [15:0] MDR_Data, input select, output [15:0] data);
 
-always_comb
-begin
-	if(select)
-		data <= MDR_Data;
-	else
-		data <= 16'bz;
-end	
+assign data = select ? MDR_Data : 16'bz;
 endmodule
