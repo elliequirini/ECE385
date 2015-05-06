488cbaaa7ef19d78760b27de8e2b273b239b2f3b
threeset3 <threeset3@gmail.com>
2015-05-06 16:03:48 -0500

    new spirtes

--- kitty_sprites/convert.py
+++ kitty_sprites/convert.py
@@ -4,6 +4,7 @@ import sys
 argv = sys.argv
 if len(argv) != 2:
 	print "Invalid arguments"
+	sys.exit()
 
 
 img = Image.open(argv[1] + '.png')
@@ -13,19 +14,27 @@ size = img.size
 f = open(argv[1] + '.txt', 'w')
 f.write('{')
 
-hashtable = {}
-count = 0
+hashtable = {(255, 255, 255, 0): 0,	# backgruond
+		   (0, 0, 0, 255): 1,		# black
+		   (255, 174, 201, 255): 2,	# pink
+		   (74, 74, 74, 255): 3,	# grey
+		   (181, 230, 29, 255): 4,	# green
+		   (255, 255, 255, 255): 5,	# white
+		   (94, 0, 14, 255): 6,		# dark red
+		   (136, 0, 21, 255): 7, 	# red
+		   (217, 0, 33, 255): 8,	# other red
+		   (0, 0, 0, 0): 0
+		   }
 
 for y in range(size[1]):
 	f.write('{')
 
 	for x in range(size[0]):
-
 		if pixel[x,y] not in hashtable:
-			hashtable[pixel[x,y]] = count
-			count = count + 1
-
-		f.write(str(hashtable[pixel[x,y]]))
+			print "Invalid color: " + str(pixel[x,y])
+			f.write("x")
+		else:
+			f.write(str(hashtable[pixel[x,y]]))
 		if x != size[0] -1: f.write(',')
 
 	f.write('}')
